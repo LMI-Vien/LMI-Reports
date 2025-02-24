2320d0ed83112beace2478a591171290db89944f
LMI-Louise <louise.lifestrong@gmail.com>
2025-02-24 15:32:29 +0800

    add year maintenance

---
+++ app/Controllers/Cms/Year.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers\Cms;
+
+use App\Controllers\BaseController;
+
+class Year extends BaseController
+{
+    protected $session;
+
+    public function __construct()
+	{
+	    $this->session = session();
+	    if (!$this->session->get('sess_uid')) {
+	        redirect()->to(base_url('cms/login'))->send();
+	        exit;
+	    }
+	}
+
+	public function index()
+	{
+		$data['meta'] = array(
+			"title"         =>  "Year",
+			"description"   =>  "Year",
+			"keyword"       =>  ""
+		);
+		$data['title'] = "Year";
+		$data['PageName'] = 'Year';
+		$data['PageUrl'] = 'Year';
+		$data['buttons'] = ['add', 'search'];
+		$data['content'] = "cms/year/year.php";
+		$data['session'] = session(); //for frontend accessing the session data
+		$data['js'] = array(
+				"assets/js/bootstrap.min.js",
+				"assets/js/adminlte.min.js",
+				"assets/js/moment.js",
+				"assets/js/xlsx.full.min.js"
+                    );
+        $data['css'] = array(
+        		"assets/css/bootstrap.min.css",
+        		"assets/css/adminlte.min.css",
+        		"assets/css/all.min.css",
+        		"assets/cms/css/main_style.css",//css sa style ni master Vien
+        		"assets/css/style.css"
+                    );
+		return view("cms/layout/template", $data);	
+	}
+
+}
