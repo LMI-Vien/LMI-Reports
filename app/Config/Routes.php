120b2600e545d4106f43559d1b22ebaa3bae0d00
Jan <janjalanie@hotmail.com>
2025-03-03 10:21:28 +0800

    Updated Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -157,8 +157,4 @@ $routes->group('cms/', static function ($routes) {
 	});
 
     $routes->post('global_controller', 'Cms\Global_controller::index');
-<<<<<<< Updated upstream
 });
-=======
-});
->>>>>>> Stashed changes
