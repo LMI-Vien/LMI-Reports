e923279c81b69202302ccdb1a04220c024941639
LMI-Louise <louise.lifestrong@gmail.com>
2025-03-19 11:44:56 +0800

    Add Logo - site and cms

--- app/Views/cms/layout/header_login.php
+++ app/Views/cms/layout/header_login.php
@@ -9,6 +9,8 @@
         <title><?=$meta['title']?></title>
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <link rel="icon" href="<?= base_url('assets/img/lmi_logo.ico') ?>" type="image/x-icon">
+        
         <script type="text/javascript" src="<?= base_url();?>assets/js/jquery-3.7.1.min.js" ></script>
         <script type="text/javascript" src="<?= base_url();?>assets/js/cms_custom.js" ></script>
         
--- app/Views/site/layout/header_login.php
+++ app/Views/site/layout/header_login.php
@@ -8,6 +8,7 @@
         <meta property="og:description" content="<?=$meta['description']?>" />
         <title><?=$meta['title']?></title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <link rel="icon" href="<?= base_url('assets/img/lmi_logo.ico') ?>" type="image/x-icon">
 
         <script type="text/javascript" src="<?= base_url();?>assets/js/jquery-3.7.1.min.js" ></script>
         <script type="text/javascript" src="<?= base_url();?>assets/js/cms_custom.js" ></script>
