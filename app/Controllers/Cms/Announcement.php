dee6ede0ed3b3652631ee9d28bc13a97d71c68bd
LMI-Louise <louise.lifestrong@gmail.com>
2025-04-21 13:56:17 +0800

    Announcement Module - crud

---
+++ app/Controllers/Cms/Announcement.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controllers\Cms;
+
+use App\Controllers\BaseController;
+
+class Announcement extends BaseController
+{
+    protected $session;
+
+    public function __construct()
+	{
+	    $this->session = session();
+	    if (!$this->session->get('sess_uid')) {
+	        redirect()->to(base_url('cms/login'))->send();
+	        exit;
+	    }
+	}
+
+	public function index()
+	{
+		$data['meta'] = array(
+			"title"         =>  "Announcements",
+			"description"   =>  "Announcements",
+			"keyword"       =>  ""
+		);
+		$data['title'] = "Announcements";
+		$data['PageName'] = 'Announcements';
+		$data['PageUrl'] = 'Announcements';
+		$data['buttons'] = ['add', 'search', 'filter'];
+		$data['content'] = "cms/maintenance/announcement/announcement.php";
+		$data['session'] = session();
+
+		$data['js'] = array(
+				"assets/js/bootstrap.min.js",
+				"assets/js/adminlte.min.js",
+				"assets/js/moment.js",
+				"assets/js/xlsx.full.min.js"
+                    );
+        $data['css'] = array(
+        		"assets/css/bootstrap.min.css",
+        		"assets/css/adminlte.min.css",
+        		"assets/css/all.min.css",
+        		"assets/cms/css/main_style.css",
+        		"assets/css/style.css"
+                    );
+		return view("cms/layout/template", $data);	
+	}
+
+}
