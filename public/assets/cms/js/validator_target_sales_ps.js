b749e00fc9526922f0fe93743f0993c0f0a5115c
Jan <janjalanie@hotmail.com>
2025-05-08 13:18:55 +0800

    Comment validation for area store lookup

--- public/assets/cms/js/validator_target_sales_ps.js
+++ public/assets/cms/js/validator_target_sales_ps.js
@@ -162,7 +162,7 @@ self.onmessage = async function(e) {
                 }
 
                 let matched = ba_checklist[location.toLowerCase()];
-                if (!matched?.store_id) addErrorLog("Invalid store not tagged to any area");
+                //if (!matched?.store_id) addErrorLog("Invalid store not tagged to any area");
     
                 if (!invalid) {
                     valid_data.push({
