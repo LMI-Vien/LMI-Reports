1cace619864647385489a63fd97cef1116499406
LMI-Louise <louise.lifestrong@gmail.com>
2025-05-20 10:40:33 +0800

    Formula upon hover - sales performance per area

--- app/Views/site/store/perf-per-area/sales_performance_per_area.php
+++ app/Views/site/store/perf-per-area/sales_performance_per_area.php
@@ -73,10 +73,35 @@
                                         <th class="tbl-title-field">LY Scan Data</th>
                                         <th class="tbl-title-field">Actual Sales Report</th>
                                         <th class="tbl-title-field">Target</th>
-                                        <th class="tbl-title-field">% Achieved</th>
-                                        <th class="tbl-title-field">% Growth</th>
-                                        <th class="tbl-title-field">Balance To Target</th>
-                                        <th class="tbl-title-field">Target Per Remaining Days</th>
+                                        <th class="tbl-title-field">% Achieved 
+                                            <span class="tooltip-text">
+                                                FORMULA:
+                                                <br>
+                                                Actual Sales Report / Target * 100 
+                                            </span>
+                                        </th>
+                                        </th>
+                                        <th class="tbl-title-field">% Growth
+                                            <span class="tooltip-text">
+                                                FORMULA:
+                                                <br>
+                                                Actual Sales Report / LY Scan Data * 100
+                                            </span>
+                                        </th>
+                                        <th class="tbl-title-field">Balance To Target
+                                            <span class="tooltip-text">
+                                                FORMULA:
+                                                <br>
+                                                Target - Actual Sales Report
+                                            </span>
+                                        </th>
+                                        <th class="tbl-title-field">Target Per Remaining Days
+                                            <span class="tooltip-text">
+                                                FORMULA:
+                                                <br>
+                                                Balance to Target / remaining days in a month
+                                            </span>
+                                        </th>
 
                                     </tr>
                                 </thead>
